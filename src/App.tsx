90bf09a15d99ed8568b7120f2a03eed2e21f92a9
nhattien7701vn <nhattien7701vn@gmail.com>
2023-06-16 17:49:22 +0700

    import axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react"
 import Main from "./assets/layouts/Main/Main"
 import Sidebar from "./assets/layouts/Sidebar/Sidebar"
 
 function App() {
+  useEffect(()=>{
+    (async()=>{
+      try {
+      } catch (error) {
+        
+      }
+    })
+  })
   return (
     <>
       <Sidebar />
